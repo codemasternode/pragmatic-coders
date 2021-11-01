dcaaa95640ab3c66cd10f15f249b23d38e757904
marcin_warzybok <marcinwarzybok@outlook.com>
2021-11-01 19:20:44 +0100

    Test(zadanie-2): Prepare tests for create skill feature
    Add tests:
    - should create skill when skill rate is valid
    - should return error when skill rate is not valid

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -2,9 +2,23 @@ export type HttpErrorResponseBody = { message: string; kind?: string };
 
 export class HttpErrorResponse<
   T extends HttpErrorResponseBody = HttpErrorResponseBody
-> extends Error {
+  > extends Error {
   constructor(readonly statusCode: number, readonly body: T) {
     super(body.message);
+    Object.setPrototypeOf(this, HttpErrorResponse.prototype);
+  }
+}
+
+export type TransformErrorResponseBody = { message: string; validationErrors: unknown }
+
+export class TransformError<
+  T extends TransformErrorResponseBody = TransformErrorResponseBody
+  > extends Error {
+  validationErrors: unknown
+
+  constructor(readonly statusCode: number, readonly body: T) {
+    super(body.message);
+    this.validationErrors = body.validationErrors
     Object.setPrototypeOf(this, HttpErrorResponse.prototype);
   }
 }
